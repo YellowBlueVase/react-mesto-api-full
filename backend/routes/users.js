a61dab93462aa00d767af1832f3a4ad401698ccc
Kirill Shilin <kvshilin@icloud.com>
2023-02-26 00:49:37 +0300

    new

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,10 +3,21 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
-  getAllUsers, getUser, getProfile, updateProfile, updateAvatar,
+  getAllUsers, getUser, getProfile, updateProfile, updateAvatar, login, createUser,
 } = require('../controllers/users');
 
 Joi.objectId = require('joi-objectid')(Joi);
+
+const celebrateUser = {
+  body: Joi.object().keys({
+    _id: Joi.objectId(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }).unknown(true),
+};
 
 router.get('/users', auth, getAllUsers);
 
@@ -36,5 +47,9 @@ router.patch('/users/me/avatar', celebrate(
     }).unknown(true),
   },
 ), auth, updateAvatar);
+
+router.post('/signin', celebrate(celebrateUser), login);
+
+router.post('/signup', celebrate(celebrateUser), createUser);
 
 module.exports = router;
